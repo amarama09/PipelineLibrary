af699b8550147f660494e4d1ec5a930ddba3dbfa
amarama <srinuamar9@gmail.com>
2018-02-16 13:09:04 -0800

    one more thing...3

--- vars/SayHello.groovy
+++ vars/SayHello.groovy
@@ -1,6 +1,14 @@
-def call (String name='you'){
+def call(String name='you'){
 
   echo "hello ${name}"
 
-  
+
+}
+
+def info(message) {
+    echo "INFO: ${message}"
+}
+
+def warning(message) {
+    echo "WARNING: ${message}"
 }
